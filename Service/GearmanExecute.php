b97ec0c144bed39b321d1eb56ea62816b888b2cd
grimbode <kevin.faulhaber@gmail.com>
2016-04-15 15:14:22 +0200

    updates

--- Service/GearmanExecute.php
+++ Service/GearmanExecute.php
@@ -77,11 +77,10 @@ class GearmanExecute extends AbstractGearmanService
                 'minimum_execution_time' => null,
                 'timeout'                => null,
             ))
-            ->setAllowedTypes("gearman", array(
-                'iterations'             => array('null', 'scalar'),
-                'minimum_execution_time' => array('null', 'scalar'),
-                'timeout'                => array('null', 'scalar'),
-            ))
+            ->setAllowedTypes('iterations', array('null', 'scalar'))
+            ->setAllowedTypes('minimum_execution_time', array('null', 'scalar'))
+            ->setAllowedTypes('timeout', array('null', 'scalar'))
+
         ;
     }
 
